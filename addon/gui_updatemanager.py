b48c8098d702245a829649aea50384cc04495043
Abdo <abdo@abdnh.net>
2023-05-06 02:32:07 +0300

    Replace Anki Palace

--- addon/gui_updatemanager.py
+++ addon/gui_updatemanager.py
@@ -299,7 +299,7 @@ class SettingsDialog(QDialog):
         elif site == "video":
             openLink("https://youtu.be/5XAq0KpU3Jc")
         elif site == "palace":
-            openLink("https://courses.ankipalace.com/?utm_source=anking_bg_add-on&utm_medium=anki_add-on&utm_campaign=mastery_course")
+            openLink("https://www.theanking.com/anki-mastery-course/?utm_source=anking_bg_add-on&utm_medium=anki_add-on&utm_campaign=mastery_course")
 
     def random(self):
         f = self.form
@@ -366,10 +366,10 @@ def create_get_help_submenu(parent: QMenu) -> QMenu:
     menu_options = [
         (
             "Online Mastery Course",
-            "https://courses.ankipalace.com/?utm_source=anking_bg_add-on&utm_medium=anki_add-on&utm_campaign=mastery_course",
+            "https://www.theanking.com/anki-mastery-course/?utm_source=anking_bg_add-on&utm_medium=anki_add-on&utm_campaign=mastery_course",
         ),
-        ("Daily Q and A Support", "https://www.ankipalace.com/memberships"),
-        ("1-on-1 Tutoring", "https://www.ankipalace.com/tutoring"),
+        ("Daily Q and A Support", "https://www.theanking.com/anking-memberships"),
+        ("1-on-1 Tutoring", "https://www.theanking.com/anking-tutoring"),
     ]
     submenu = QMenu(submenu_name, parent)
     for name, url in menu_options:
